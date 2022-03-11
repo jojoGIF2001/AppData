51256a50fee0339f975d2f70b545a3760868dc33
jojoGIF2001 <dietrichjohannes295@gmail.com>
2022-03-11 20:15:56 +0100

    v.1.1.8
    11.03.2022

---
+++ Test/index.js
@@ -0,0 +1,12 @@
+// <>
+
+alert("Gieb deinen Namen ein!");
+
+var A = prompt("Dein Name", "Johannes");
+
+alert("Hallo " + A);
+
+function getName(){
+    console.log("Hallo " + A);
+    return A;
+}
