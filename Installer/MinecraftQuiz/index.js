dc5db86c1c1ad6e4a17df04e0ec43fe54a7853f1
jojoGIF2001 <dietrichjohannes295@gmail.com>
2022-04-14 21:55:55 +0200

    2.0.1

---
+++ Installer/MinecraftQuiz/index.js
@@ -0,0 +1,17 @@
+// <>
+
+
+function download(){
+    var Download = confirm("Download Starten?");
+
+    if(Download == true){
+        window.location.href= 'https://jojogif2001.github.io/AppData/apk/MinecraftQuiz.apk';
+        install();
+    }
+
+
+}
+
+function install(){
+        window.open('https://docs.google.com/document/d/1bkzdo5a2KG5z682ai3N2XmAgLJbIktw8uDljJeHlWuQ/edit?usp=sharing');
+    }
